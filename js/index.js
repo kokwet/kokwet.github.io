160b713a1031b8858e8d176dfb285e8932b2ca7e
mrotich <microtich@gmail.com>
2015-02-22 20:13:34 -0500

    pre-release: version 0

--- js/index.js
+++ js/index.js
@@ -79,7 +79,7 @@ $(function() {
                         $('#success > .alert-success').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
                         .append( "</button>");
                         $('#success > .alert-success')
-                        .append("<strong>Your message has been sent. </strong>");
+                        .append("Wohoo, your message has been submitted. We'll be in touch soon. Meanwhile, check out our social media pages for the latest!");
                         $('#success > .alert-success')
                         .append('</div>');
 
@@ -100,5 +100,16 @@ $(function() {
                 });
             }
         });
+    });
+});
+
+//========================================================================== 
+// Grid alignment script
+$(function(){
+    var container = document.querySelector('#programs-collapse');
+    var msnry;
+    // initialize Masonry after all images have loaded
+    imagesLoaded( container, function() {
+        msnry = new Masonry( container, {columnWidth: 0,itemSelector: '.program-wrapper'});
     });
 });
